7f63936f999be396d75e62e48c65d5d60f1f3d2c
Diegopal2608 <diegopal2608@gmail.com>
2024-10-18 14:51:04 -0600

    Create practica 10.c

---
+++ practica 10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+  int arr[3][3]={{1,2,3},{4,5,6}};
+  int i, j;
+  printf("Imprimir matriz\m");
+
+  for (i = 0; i < 3; i++)
+{
+   printf ("%d | " , arr[i][j];
+}
+printf("\n");
+}
+return 0;
+
